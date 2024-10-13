aff5ed27cae075fe655057142d41b1012f1fd851
Prozzo86 <bogdanovic.darijan@gmail.com>
2024-10-13 21:23:18 +0200

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int konacnaSuma = 43;
+        int uneseniBroj;
+        int zbrojSvih = 0;
+        int brojPutaUnosa = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        
+        while (zbrojSvih < konacnaSuma) {
+            System.out.println("Molim unesite neki broj: ");
+            uneseniBroj = scanner.nextInt();
+
+
+            if (uneseniBroj == 0) {
+                System.out.println("Unos prekinut. Trenutni zbroj je: " + zbrojSvih);
+                break;
+            }
+
+            if (uneseniBroj < 0) {
+                System.out.println("Negativni brojevi se ignoriraju. Pokušajte ponovo.");
+                continue;
+            }
+
+            zbrojSvih += uneseniBroj;
+            brojPutaUnosa++;
+        }
+
+        if (zbrojSvih >= konacnaSuma) {
+            System.out.println("Konačna suma je dostignuta ili premašena!");
+            System.out.println("Broj unosa je: " + brojPutaUnosa);
+            System.out.println("Konačni zbroj unesenih brojeva je: " + zbrojSvih);
+        }
     }
 }
